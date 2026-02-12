27b4eeb8d7bb50596410b411b2ca1edcf094e533
ArtyomVitkovski <a.vitkouski@pxlnd.com>
2026-02-12 10:35:36 +0300

    changed asset provider method naming

--- Assets/_Project/Scripts/Core/Services/AssetsProvider/AssetsProvider.cs
+++ Assets/_Project/Scripts/Core/Services/AssetsProvider/AssetsProvider.cs
@@ -5,6 +5,6 @@ namespace _Project.Scripts.Core.Services.AssetsProvider
 {
     public interface IAssetsProvider<TAsset, in TContainer>
     {
-        public UniTask<TAsset> GetTexture(TContainer assetContainer, CancellationToken token);
+        public UniTask<TAsset> GetAsset(TContainer assetContainer, CancellationToken token);
     }
 }
